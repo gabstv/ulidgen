846062881835cac31d76121190a0ddb57b4b784e
Gabriel Ochsenhofer <gabriel.ochsenhofer@gmail.com>
2023-10-20 11:01:41 -0300

    add files

---
+++ README.md
@@ -0,0 +1,13 @@
+# ulidgen
+
+A minimal binary executable of [oklog/ulid](https://github.com/oklog/ulid).
+
+## Installation
+
+### Go Install
+
+With Go 1.20 or greater installed, run:
+
+```sh
+go install github.com/gabstv/ulidgen@latest
+```
---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"crypto/rand"
+	"os"
+	"time"
+
+	"github.com/oklog/ulid/v2"
+)
+
+const (
+	version = "1.0.0"
+)
+
+func main() {
+	if len(os.Args) == 2 && (os.Args[1] == "version" || os.Args[1] == "-v" || os.Args[1] == "--version") {
+		print(version)
+		return
+	}
+
+	if len(os.Args) != 1 {
+		print("Usage: ulid")
+
+		os.Exit(1)
+	}
+
+	id := ulid.MustNew(ulid.Timestamp(time.Now()), rand.Reader)
+
+	print(id.String())
+}
